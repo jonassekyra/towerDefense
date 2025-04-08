c96ff5b147cec6661c3fd58ffea7302f64b9234f
jonas <jonassekyra@gmail.com>
2025-04-08 20:53:54 +0200

    Adding constructor with set values and paint method that draws the map of the level.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GamePanel extends JPanel {
+    Level level = new Level();
 
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(750, 750));
+        this.setVisible(true);
+    }
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        level.drawLevel(g2d);
+
+    }
 }
