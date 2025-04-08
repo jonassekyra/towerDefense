ae3994c34de284ab630891433724e9f456df2d8b
jonas <jonassekyra@gmail.com>
2025-04-08 20:57:28 +0200

    Adding a constructor.

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,4 +1,14 @@
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
+    GamePanel gamePanel;
+    public MainFrame() {
+        gamePanel = new GamePanel();
+        this.add(gamePanel);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
 }
