241581b678ba4e02307f1315ecbf442c21b70abd
jonas <jonassekyra@gmail.com>
2025-04-10 14:51:14 +0200

    Improving enemies turning, some kind of condition is still missing

--- src/Movement.java
+++ src/Movement.java
@@ -1,17 +1,16 @@
 public class Movement {
-    Enemy enemy;
-    Level level;
 
     public void move(Enemy enemy, Level level) {
-        if (level.getTiles()[enemy.getX() + 1][enemy.getY()].equals(1)) {
+        enemy.setLastX(enemy.getX());
+        enemy.setLastY(enemy.getY());
+        if (level.getMap()[enemy.getX() + 1][enemy.getY()] == 1 && (enemy.getX() + 1) != enemy.getLastX()) {
             enemy.setX(enemy.getX() + 1);
-        } else if (level.getTiles()[enemy.getX()][enemy.getY() + 1].equals(1)) {
-            enemy.setX(enemy.getX() - 1);
-        } else if (level.getTiles()[enemy.getX() - 1][enemy.getY()].equals(1)) {
+        } else if (level.getMap()[enemy.getX()][enemy.getY() + 1] == 1 && (enemy.getY() + 1) != enemy.getLastY()) {
             enemy.setY(enemy.getY() + 1);
-        } else if (level.getTiles()[enemy.getX()][enemy.getY() - 1].equals(1)) {
-            enemy.setY(enemy.getY()-1);
+        } else if (level.getMap()[enemy.getX()][enemy.getY() - 1] == 1&& (enemy.getY() - 1) != enemy.getLastY()) {
+            enemy.setY(enemy.getY() - 1);
+        } else if (level.getMap()[enemy.getX() - 1][enemy.getY()] == 1 && (enemy.getX() - 1)!= enemy.getLastX()) {
+            enemy.setX(enemy.getX() - 1);
         }
-
     }
 }
