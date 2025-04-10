930d2f3262a7503c69cfe5ba7a102328fec4c252
jonas <jonassekyra@gmail.com>
2025-04-10 14:55:29 +0200

    Adding a Timer which is used every 16 milliseconds(60 frames per second), and action perform method and update method, not yet sure if it's going to be used.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,18 +1,33 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GamePanel extends JPanel {
     Level level = new Level();
+    Timer timer;
+
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(750, 750));
         this.setVisible(true);
+
+        timer = new Timer(16, this::actionPerformed);
+        timer.start();
     }
     @Override
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
         level.drawLevel(g2d);
+        level.drawEnemy(g2d);
 
+    }
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
+    private void updateGame(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        level.drawEnemy(g2d);
     }
 }
