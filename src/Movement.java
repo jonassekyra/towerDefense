a6e5d578d815f62dd600a1471417dfe81412b47b
jonas <jonassekyra@gmail.com>
2025-04-14 19:33:41 +0200

    Tried a lot of options and it still isn't working. I will try to rewrite it

--- src/Movement.java
+++ src/Movement.java
@@ -1,16 +1,25 @@
 public class Movement {
-
+/*
     public void move(Enemy enemy, Level level) {
         enemy.setLastX(enemy.getX());
         enemy.setLastY(enemy.getY());
-        if (level.getMap()[enemy.getX() + 1][enemy.getY()] == 1 && (enemy.getX() + 1) != enemy.getLastX()) {
+        Position currentPosition = new Position(enemy.getX(),enemy.getY());
+        Position lastPosition = new Position(enemy.getX(),enemy.getY());
+        //&& (enemy.getX() + 1) != enemy.getLastX()
+        if (level.getMap()[enemy.getX() + 1][enemy.getY()] == 1 && !lastPosition.equals(currentPosition.right())) {
             enemy.setX(enemy.getX() + 1);
-        } else if (level.getMap()[enemy.getX()][enemy.getY() + 1] == 1 && (enemy.getY() + 1) != enemy.getLastY()) {
+            //(enemy.getY() + 1) != enemy.getLastY()
+            //!lastPosition.equals(new Position(enemy.getX(), enemy.getY() + 1))
+        } else if (level.getMap()[enemy.getX()][enemy.getY() + 1] == 1 && (enemy.getY() + 1) != enemy.getLastY() && enemy.getY() != enemy.getLastY()) {
             enemy.setY(enemy.getY() + 1);
-        } else if (level.getMap()[enemy.getX()][enemy.getY() - 1] == 1&& (enemy.getY() - 1) != enemy.getLastY()) {
+            //(enemy.getY() - 1) != enemy.getLastY()
+        } else if (level.getMap()[enemy.getX()][enemy.getY() - 1] == 1 &&  !lastPosition.equals(new Position(enemy.getX(), enemy.getY() - 1))) {
             enemy.setY(enemy.getY() - 1);
-        } else if (level.getMap()[enemy.getX() - 1][enemy.getY()] == 1 && (enemy.getX() - 1)!= enemy.getLastX()) {
+            //(enemy.getX() - 1)!= enemy.getLastX()
+        } else if (level.getMap()[enemy.getX() - 1][enemy.getY()] == 1 && !lastPosition.equals(currentPosition.left())) {
             enemy.setX(enemy.getX() - 1);
         }
     }
+    
+ */
 }
