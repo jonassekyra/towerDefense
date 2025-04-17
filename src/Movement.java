7e6fb28e6c5e7660a2e0deab671e3d37daed6387
jonas <jonassekyra@gmail.com>
2025-04-17 18:49:17 +0200

    Redoing move method, adding a delay between each move and adding a condition that checks if the enemy is at the end.

--- src/Movement.java
+++ src/Movement.java
@@ -20,6 +20,49 @@ public class Movement {
             enemy.setX(enemy.getX() - 1);
         }
     }
-    
+
  */
+
+    public void move(Enemy enemy, Level level) {
+
+        Position lastPosition = new Position(enemy.getX(), enemy.getY());
+        Position currentPosition = new Position(enemy.getX(), enemy.getY());
+        Position finishPosition = new Position(enemy.getEndX(), enemy.getEndY());
+        if (!currentPosition.equals(finishPosition)) {
+            if (level.getMap()[enemy.getX() + 1][enemy.getY()] == 1 && !lastPosition.equals(currentPosition.right())) {
+                // This sets the value of the location to a high number that is unreachable so it cannot be accessed.
+                System.out.println(currentPosition.right());
+                level.getMap()[enemy.getX()][enemy.getY()] = 10;
+                enemy.setX(enemy.getX() + 1);
+
+            } else if (level.getMap()[enemy.getX() - 1][enemy.getY()] == 1 && !lastPosition.equals(currentPosition.left())) {
+                level.getMap()[enemy.getX()][enemy.getY()] = 10;
+                System.out.println(currentPosition.left());
+                enemy.setX(enemy.getX() - 1);
+
+            } else if (level.getMap()[enemy.getX()][enemy.getY() + 1] == 1 && !lastPosition.equals(currentPosition.down())) {
+                level.getMap()[enemy.getX()][enemy.getY()] = 10;
+                System.out.println(currentPosition.down());
+                enemy.setY(enemy.getY() + 1);
+
+            } else if (level.getMap()[enemy.getX()][enemy.getY() - 1] == 1 && !lastPosition.equals(currentPosition.up())) {
+                level.getMap()[enemy.getX()][enemy.getY()] = 10;
+                System.out.println(currentPosition.up());
+                enemy.setY(enemy.getY() - 1);
+
+            }
+
+
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }else {
+            System.out.println("konec");
+        }
+
+    }
+
+
 }
