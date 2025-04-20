5dc3bbe2ef6b8afd534a2b38a0a8e17018bfb814
jonas <jonassekyra@gmail.com>
2025-04-20 12:59:00 +0200

    Adding cost of tower

--- src/Tower/Tower.java
+++ src/Tower/Tower.java
@@ -7,4 +7,12 @@ public abstract class Tower {
     protected int damage;
     protected Time attackSpeed;
     protected BufferedImage image;
+    protected int cost;
+
+    public Tower(int damage, Time attackSpeed, BufferedImage image, int cost) {
+        this.damage = damage;
+        this.attackSpeed = attackSpeed;
+        this.image = image;
+        this.cost = cost;
+    }
 }
