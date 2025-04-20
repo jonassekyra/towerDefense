d51c4aea4ce8b55b2eeb54d3b81d62d80551e3f9
jonas <jonassekyra@gmail.com>
2025-04-20 11:29:11 +0200

    Redoing move method again, now it checks if the current position is the finish position, if not it checks where can it go and also if the enemy already been there.

--- src/Movement.java
+++ src/Movement.java
@@ -1,66 +1,40 @@
 public class Movement {
-/*
-    public void move(Enemy enemy, Level level) {
-        enemy.setLastX(enemy.getX());
-        enemy.setLastY(enemy.getY());
-        Position currentPosition = new Position(enemy.getX(),enemy.getY());
-        Position lastPosition = new Position(enemy.getX(),enemy.getY());
-        //&& (enemy.getX() + 1) != enemy.getLastX()
-        if (level.getMap()[enemy.getX() + 1][enemy.getY()] == 1 && !lastPosition.equals(currentPosition.right())) {
-            enemy.setX(enemy.getX() + 1);
-            //(enemy.getY() + 1) != enemy.getLastY()
-            //!lastPosition.equals(new Position(enemy.getX(), enemy.getY() + 1))
-        } else if (level.getMap()[enemy.getX()][enemy.getY() + 1] == 1 && (enemy.getY() + 1) != enemy.getLastY() && enemy.getY() != enemy.getLastY()) {
-            enemy.setY(enemy.getY() + 1);
-            //(enemy.getY() - 1) != enemy.getLastY()
-        } else if (level.getMap()[enemy.getX()][enemy.getY() - 1] == 1 &&  !lastPosition.equals(new Position(enemy.getX(), enemy.getY() - 1))) {
-            enemy.setY(enemy.getY() - 1);
-            //(enemy.getX() - 1)!= enemy.getLastX()
-        } else if (level.getMap()[enemy.getX() - 1][enemy.getY()] == 1 && !lastPosition.equals(currentPosition.left())) {
-            enemy.setX(enemy.getX() - 1);
-        }
-    }
-
- */
 
     public void move(Enemy enemy, Level level) {
 
-        Position lastPosition = new Position(enemy.getX(), enemy.getY());
         Position currentPosition = new Position(enemy.getX(), enemy.getY());
         Position finishPosition = new Position(enemy.getEndX(), enemy.getEndY());
-        if (!currentPosition.equals(finishPosition)) {
-            if (level.getMap()[enemy.getX() + 1][enemy.getY()] == 1 && !lastPosition.equals(currentPosition.right())) {
-                // This sets the value of the location to a high number that is unreachable so it cannot be accessed.
-
-                level.getMap()[enemy.getX()][enemy.getY()] = 10;
-                enemy.setX(enemy.getX() + 1);
-
-            } else if (level.getMap()[enemy.getX() - 1][enemy.getY()] == 1 && !lastPosition.equals(currentPosition.left())) {
-                level.getMap()[enemy.getX()][enemy.getY()] = 10;
-
-                enemy.setX(enemy.getX() - 1);
-
-            } else if (level.getMap()[enemy.getX()][enemy.getY() + 1] == 1 && !lastPosition.equals(currentPosition.down())) {
-                level.getMap()[enemy.getX()][enemy.getY()] = 10;
-
-                enemy.setY(enemy.getY() + 1);
-
-            } else if (level.getMap()[enemy.getX()][enemy.getY() - 1] == 1 && !lastPosition.equals(currentPosition.up())) {
-                level.getMap()[enemy.getX()][enemy.getY()] = 10;
-
-                enemy.setY(enemy.getY() - 1);
-
-            }
-
-
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }else {
+        if (currentPosition.equals(finishPosition)) {
             System.out.println("konec");
+            enemy.setHasEnded(true);
+            return;
         }
+        if (level.getMap()[enemy.getX() + 1][enemy.getY()] == 1 && !enemy.getVisitedLocations().contains(currentPosition.right())) {
+
+
+            enemy.getVisitedLocations().add(currentPosition.right());
+            enemy.setX(enemy.getX() + 1);
+
+        } else if (level.getMap()[enemy.getX() - 1][enemy.getY()] == 1 && !enemy.getVisitedLocations().contains(currentPosition.left())) {
+
+            enemy.getVisitedLocations().add(currentPosition.left());
+            enemy.setX(enemy.getX() - 1);
+
+
+        } else if (level.getMap()[enemy.getX()][enemy.getY() + 1] == 1 && !enemy.getVisitedLocations().contains(currentPosition.down())) {
+            enemy.getVisitedLocations().add(currentPosition.down());
+            enemy.setY(enemy.getY() + 1);
+
+        } else if (level.getMap()[enemy.getX()][enemy.getY() - 1] == 1 && !enemy.getVisitedLocations().contains(currentPosition.up())) {
+            enemy.getVisitedLocations().add(currentPosition.up());
+            enemy.setY(enemy.getY() - 1);
+        }
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 
