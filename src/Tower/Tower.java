f9556ec11d26f93ad017a1a8a3db6234c5c687be
jonas <jonassekyra@gmail.com>
2025-04-20 12:35:23 +0200

    Adding attributes

---
+++ src/Tower/Tower.java
@@ -0,0 +1,10 @@
+package Tower;
+
+import java.awt.image.BufferedImage;
+import java.sql.Time;
+
+public abstract class Tower {
+    protected int damage;
+    protected Time attackSpeed;
+    protected BufferedImage image;
+}
