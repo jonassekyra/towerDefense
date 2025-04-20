accb6f2e63dda92c54aee51356c801a6affc77c1
jonas <jonassekyra@gmail.com>
2025-04-20 12:58:24 +0200

    Adding money and game state

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,20 @@
 public class Game {
+    private int money;
+    private GameState gameState;
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public void setGameState(GameState gameState) {
+        this.gameState = gameState;
+    }
 }
---
+++ src/GameState.java
@@ -0,0 +1,3 @@
+public enum GameState {
+    DEFAULT,PLACING_TOWER
+}
