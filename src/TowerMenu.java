d9cc418ca43b89fe0b5aec73e58d741624fc1081
jonas <jonassekyra@gmail.com>
2025-04-20 11:31:38 +0200

    Adding box layout, 2 buttons, 2 actionListeners and 2 gaps.

--- src/TowerMenu.java
+++ src/TowerMenu.java
@@ -1,2 +1,33 @@
-public class TowerMenu {
+import javax.swing.*;
+import java.awt.*;
+
+public class TowerMenu extends JPanel {
+    JButton normalTower = new JButton("Normal Tower");
+    JButton slowTower = new JButton("Slow Tower");
+
+    public TowerMenu() {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setVisible(true);
+        setBackground(Color.GRAY);
+
+        //normalTower.setBounds(10, 10, 200, 30);
+        //slowTower.setBounds(10, 50, 200, 300);
+        add(Box.createRigidArea(new Dimension(0, 20)));
+        normalTower.setPreferredSize(new Dimension(200, 30));
+
+        slowTower.setPreferredSize(new Dimension(200, 30));
+        slowTower.setVerticalAlignment(SwingConstants.CENTER);
+        slowTower.setHorizontalAlignment(SwingConstants.CENTER);
+
+        add(normalTower);
+        add(Box.createRigidArea(new Dimension(0, 20)));
+        add(slowTower);
+        normalTower.addActionListener(e ->{
+
+        });
+        slowTower.addActionListener(e ->{
+
+        });
+    }
+
 }
