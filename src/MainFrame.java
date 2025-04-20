f58e184bf8df8a7bf15a4f74e7b64626843231b1
jonas <jonassekyra@gmail.com>
2025-04-20 11:26:14 +0200

    Adding Border layout, title and tower menu to MainFrame.java

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,14 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
     GamePanel gamePanel;
+    TowerMenu towerMenu;
     public MainFrame() {
         gamePanel = new GamePanel();
-        this.add(gamePanel);
+        towerMenu = new TowerMenu();
+        this.setLayout(new BorderLayout());
+        this.add(gamePanel, BorderLayout.CENTER);
+        this.setTitle("TowerDefense");
+        this.add(towerMenu, BorderLayout.EAST);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         this.pack();
         this.setVisible(true);
         this.setLocationRelativeTo(null);
+
     }
 }
