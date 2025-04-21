2a4710a9272e4078c1b1ac2ac2383a7dc2082d4d
jonas <jonassekyra@gmail.com>
2025-04-21 15:47:37 +0200

    adding 2D array of towers and draw towers method

--- src/Level.java
+++ src/Level.java
@@ -1,3 +1,5 @@
+import Tower.Tower;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.BufferedReader;
@@ -13,11 +15,21 @@ public class Level {
     private ArrayList<Enemy> enemies = new ArrayList<>();
     private Tile[][] tiles = new Tile[9][9];
     private int[][] map = new int[9][9];
+    private Tower[][] towers = new Tower[9][9];
     Movement movement = new Movement();
+
 
     public Level() {
         createTiles();
         loadLevel();
+    }
+
+    public Tower[][] getTowers() {
+        return towers;
+    }
+
+    public void setTowers(Tower[][] towers) {
+        this.towers = towers;
     }
 
     public HashMap<String, Tile> getTileTypes() {
@@ -57,16 +69,18 @@ public class Level {
         try {
             tileTypes.put("path", new Tile(ImageIO.read(Tile.class.getResource("/Tiles/hneda.png")), TileType.PATH));
             tileTypes.put("grass", new Tile(ImageIO.read(Tile.class.getResource("/Tiles/zelena.png")), TileType.GRASS));
+
             //enemies.add(new Enemy(0,0));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
     public void loadEnemies() {
-        try(BufferedReader br = new BufferedReader(new FileReader("Enemies1"))){
+        try (BufferedReader br = new BufferedReader(new FileReader("Enemies1"))) {
             String line;
             while ((line = br.readLine()) != null) {
-                enemies.add(new Enemy());
+                enemies.add(new Enemy(15));
             }
             System.out.println(enemies.size());
         } catch (FileNotFoundException e) {
@@ -85,13 +99,13 @@ public class Level {
                 if (line.charAt(0) == '9') {
                     String[] split = line.split("=");
                     if (split[0].equals("9startPosition")) {
-                        for (Enemy enemy1 : enemies){
+                        for (Enemy enemy1 : enemies) {
                             enemy1.setX(Integer.parseInt(split[1]));
                             enemy1.setY(Integer.parseInt(split[2]));
                         }
 
                     } else {
-                        for (Enemy enemy1 : enemies){
+                        for (Enemy enemy1 : enemies) {
                             enemy1.setEndX(Integer.parseInt(split[1]));
                             enemy1.setEndY(Integer.parseInt(split[2]));
                         }
@@ -136,26 +150,41 @@ public class Level {
 
     }
 
-    public void drawEnemy(Graphics g) {
+    public void drawEnemy(Graphics g, Game game) {
 
         Graphics2D g2D = (Graphics2D) g;
         g2D.setColor(Color.BLACK);
         Iterator<Enemy> it = enemies.iterator();
         while (it.hasNext()) {
             Enemy e = it.next();
-            System.out.println(e.getX() + "," + e.getY());
-            if (e.isHasEnded()){
+            //System.out.println(e.getX() + "," + e.getY());
+            if (e.isHasEnded()) {
+
                 it.remove();
                 continue;
             }
             g2D.fillOval(e.getX() * 75, e.getY() * 75, 75, 75);
             if (e.isHasStarted()) {
-                movement.move(e, this);
+                movement.move(e, this,game);
             } else {
                 e.setHasStarted(true);
             }
         }
 
+    }
+
+    public void drawTowers(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map.length; j++) {
+                if (towers[i][j] != null) {
+                    g2d.drawImage(towers[i][j].getImage(), i * 75, j * 75, 75, 75, null);
+
+
+                }
+            }
+
+        }
     }
 
 
