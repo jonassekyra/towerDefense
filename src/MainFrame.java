fd1327b585dabae863720b2af9592c4b7c7a5ead
jonas <jonassekyra@gmail.com>
2025-04-21 15:48:11 +0200

    Creating instance of game and setting health

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -4,9 +4,13 @@ import java.awt.*;
 public class MainFrame extends JFrame {
     GamePanel gamePanel;
     TowerMenu towerMenu;
+    Game game;
     public MainFrame() {
-        gamePanel = new GamePanel();
-        towerMenu = new TowerMenu();
+        game = new Game();
+        game.setHealth(100);
+        gamePanel = new GamePanel(game);
+        towerMenu = new TowerMenu(game);
+
         this.setLayout(new BorderLayout());
         this.add(gamePanel, BorderLayout.CENTER);
         this.setTitle("TowerDefense");
