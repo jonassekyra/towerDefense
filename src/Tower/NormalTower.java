30a967626cba9cc4f2e2dd159130b30137b5f040
jonas <jonassekyra@gmail.com>
2025-04-26 23:09:37 +0200

    Adding constructor

--- src/Tower/NormalTower.java
+++ src/Tower/NormalTower.java
@@ -1,4 +1,17 @@
 package Tower;
 
+import Attack.AttackStrategy;
+import Attack.SingleAttack;
+
+import java.awt.image.BufferedImage;
+import java.sql.Time;
+
 public class NormalTower extends Tower {
+
+    public NormalTower(int damage, BufferedImage image, int rangeX, int rangeY, int posX, int posY, AttackStrategy attackStrategy) {
+        super(damage, image, rangeX, rangeY, posX, posY,new SingleAttack());
+    }
+
+    public NormalTower() {
+    }
 }
