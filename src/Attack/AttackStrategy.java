483d361d8709f9301f9f1eb58280f6f53524f506
jonas <jonassekyra@gmail.com>
2025-04-26 22:49:56 +0200

    Changing inputs

---
+++ src/Attack/AttackStrategy.java
@@ -0,0 +1,10 @@
+package Attack;
+
+import Level.Level;
+import Tower.Tower;
+
+public interface AttackStrategy {
+
+    public void attack(Level level, Tower tower);
+
+}
--- src/AttackStrategy.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-
-public interface AttackStrategy {
-
-    public void attack(List<Enemy> enemies);
-
-}
