53f28d738a4d30eac12c0276928780379b05f6db
jonas <jonassekyra@gmail.com>
2025-05-01 21:41:24 +0200

    Adding inputs.

--- src/Tower/NormalTower.java
+++ src/Tower/NormalTower.java
@@ -8,8 +8,8 @@ import java.sql.Time;
 
 public class NormalTower extends Tower {
 
-    public NormalTower(int damage, BufferedImage image, int rangeX, int rangeY, int posX, int posY, AttackStrategy attackStrategy) {
-        super(damage, image, rangeX, rangeY, posX, posY,new SingleAttack());
+    public NormalTower(int damage, BufferedImage image, int rangeX, int rangeY, int posX, int posY, AttackStrategy attackStrategy,long cooldown) {
+        super(damage, image, rangeX, rangeY, posX, posY,new SingleAttack(),cooldown);
     }
 
     public NormalTower() {
