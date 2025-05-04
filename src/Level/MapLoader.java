56fbd2cbc3fcc45e666cc6b12466f4df22659141
jonas <jonassekyra@gmail.com>
2025-05-04 16:51:05 +0200

    Moved methods createTiles(),loadLevel() from level and calling them in the constructor.

--- src/Level/MapLoader.java
+++ src/Level/MapLoader.java
@@ -1,5 +1,129 @@
 package Level;
+import Enemy.Enemy;
+import Position.Position;
+import Tiles.Tile;
+import Tiles.TileType;
+import Enemy.EnemyManager;
+import javax.imageio.ImageIO;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MapLoader {
+    private Position startPosition;
+    private Position endPosition;
 
+    public Position getStartPosition() {
+        return startPosition;
+    }
+
+
+    public Position getEndPosition() {
+        return endPosition;
+    }
+
+
+    public MapLoader() {
+        createTiles();
+        loadLevel();
+    }
+
+    HashMap<String, Tile> tileTypes = new HashMap<>();
+    int[][] map = new int[9][9];
+    private Tile[][] tiles = new Tile[9][9];
+
+    //HashMap<String, Tile>
+    public void createTiles() {
+
+        try {
+            tileTypes.put("path", new Tile(ImageIO.read(Tile.class.getResource("/Tiles/brown.png")), TileType.PATH));
+            tileTypes.put("grass", new Tile(ImageIO.read(Tile.class.getResource("/Tiles/zelena.png")), TileType.GRASS));
+
+            //enemies.add(new Enemy.Enemy(0,0));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        //return tileTypes;
+    }
+
+    //Tile[][]
+    public void loadLevel() {
+        //loadEnemies();
+        try (BufferedReader br = new BufferedReader(new FileReader("Map.txt"))) {
+            String line;
+            int row = 0;
+            while ((line = br.readLine()) != null) {
+                if (line.charAt(0) == '9') {
+                    String[] split = line.split("=");
+                    if (split[0].equals("9startPosition")) {
+                        startPosition = new Position(Integer.parseInt(split[1]), Integer.parseInt(split[2]));
+                        /*
+                        for (Enemy enemy1 : enemies) {
+                            enemy1.setX(Integer.parseInt(split[1]));
+                            enemy1.setY(Integer.parseInt(split[2]));
+                            enemy1.setHealth(100);
+                            int tempX = Integer.parseInt(split[1]);
+                            int tempY = Integer.parseInt(split[2]);
+                            enemy1.setPixelX(tempX * 75);
+                            enemy1.setPixelY(tempY * 75);
+                            enemy1.setSpeed(1);
+                        }
+
+                         */
+
+                    } else {
+                        endPosition = new Position(Integer.parseInt(split[1]), Integer.parseInt(split[2]));
+                        /*
+                        for (Enemy enemy1 : enemies) {
+                            enemy1.setEndX(Integer.parseInt(split[1]));
+                            enemy1.setEndY(Integer.parseInt(split[2]));
+                        }
+
+                         */
+
+                    }
+                } else {
+                    String[] split = line.split(" ");
+                    for (int i = 0; i < split.length; i++) {
+                        switch (split[i]) {
+                            case "0":
+                                tiles[i][row] = tileTypes.get("path");
+                                map[i][row] = 0;
+                                break;
+                            case "1":
+                                tiles[i][row] = tileTypes.get("grass");
+                                map[i][row] = 1;
+                                break;
+                        }
+                    }
+                    row++;
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public Tile[][] getTiles() {
+        return tiles;
+    }
+
+    public void setTiles(Tile[][] tiles) {
+        this.tiles = tiles;
+    }
+
+    public int[][] getMap() {
+        return map;
+    }
+
+    public void setMap(int[][] map) {
+        this.map = map;
+    }
 }
