6356a276b45a7396443bd39fd8dc2ebfb3b825b6
jonas <jonassekyra@gmail.com>
2025-05-04 18:52:13 +0200

    Calculating distance in pixels for more accurate range.

--- src/Attack/SingleAttack.java
+++ src/Attack/SingleAttack.java
@@ -1,32 +1,31 @@
 package Attack;
-
 import Enemy.Enemy;
 import Level.Level;
 import Tower.Tower;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
 public class SingleAttack implements AttackStrategy {
     public SingleAttack() {
     }
 
+
     @Override
     public void attack(Level level, Tower tower) {
-        Iterator<Enemy> it = level.getEnemies().iterator();
+        ArrayList<Enemy> enemies = level.getEnemyManager().getEnemies();
+        Iterator<Enemy> it = enemies.iterator();
         while (it.hasNext()) {
 
             Enemy enemy = it.next();
             if (!enemy.isAlive()){
                 it.remove();
             }
-            int distanceX = enemy.getX() - tower.getPosX();
-            int distanceY = enemy.getY() - tower.getPosY();
+            int distanceX = enemy.getPixelX() - (tower.getPosX() * 75);
+            int distanceY = enemy.getPixelY() - (tower.getPosY() * 75);
 
-            if (Math.abs(distanceX) < tower.getRangeX() && Math.abs(distanceY) < tower.getRangeY()) {
-                //results.add(enemy);
-                enemy.takeDamage(tower.getDamage(),level.getEnemies());
+            if (Math.abs(distanceX) < (tower.getRangeX() * 75) && Math.abs(distanceY) < (tower.getRangeY() * 75)) {
+                enemy.takeDamage(tower.getDamage(),enemies);
                 System.out.println(enemy.getHealth());
             }
         }
