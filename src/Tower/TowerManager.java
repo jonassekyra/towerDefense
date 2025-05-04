26f84249261536b286636b6c21c3cc0e83e4b036
jonas <jonassekyra@gmail.com>
2025-05-04 18:53:52 +0200

    Moving and renaming go throw towers method to TowerManage.java and updateTowers()

--- src/Tower/TowerManager.java
+++ src/Tower/TowerManager.java
@@ -1,4 +1,30 @@
 package Tower;
 
+import Level.Level;
+
 public class TowerManager {
+    private Level level;
+    private Tower[][] towers = new Tower[10][10];
+
+    public TowerManager(Level level) {
+        this.level = level;
+    }
+
+    public void updateTowers(Level level) {
+        for (int i = 0; i < level.getMap().length; i++) {
+            for (int j = 0; j < level.getMap().length; j++) {
+                if (towers[i][j] != null) {
+                    towers[i][j].canAttack(level);
+                }
+            }
+        }
+    }
+
+    public Tower[][] getTowers() {
+        return towers;
+    }
+
+    public void setTowers(Tower[][] towers) {
+        this.towers = towers;
+    }
 }
