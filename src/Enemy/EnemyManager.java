ed55d6e90b0a7e89457fb5d5d62c24559ba2b5f8
jonas <jonassekyra@gmail.com>
2025-05-10 11:34:25 +0200

    Adding spawn enemy method,that adds enemy to ArrayList of enemies if cooldown iss over

--- src/Enemy/EnemyManager.java
+++ src/Enemy/EnemyManager.java
@@ -7,8 +7,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+
 public class EnemyManager {
     private MapLoader mapLoader;
+    private long sinceLastSpawn = 0;
 
     public EnemyManager(MapLoader mapLoader) {
         this.mapLoader = mapLoader;
@@ -17,8 +19,10 @@ public class EnemyManager {
 
     }
 
-    private ArrayList<Enemy> enemies = new ArrayList<>();
 
+    private ArrayList<Enemy> enemies = new ArrayList<>();
+    private ArrayList<Enemy> loadedEnemies = new ArrayList<>();
+    int count = 0;
 
     public ArrayList<Enemy> getEnemies() {
         return enemies;
@@ -26,6 +30,20 @@ public class EnemyManager {
 
 
     public void startEndPosition() {
+        for (Enemy e : loadedEnemies) {
+            e.setSpawnCooldown(1000);
+            e.setX(mapLoader.getStartPosition().getX());
+            e.setY(mapLoader.getStartPosition().getY());
+            e.setPixelX(mapLoader.getStartPosition().getX() * 75);
+            e.setPixelY(mapLoader.getStartPosition().getY() * 75);
+            e.setEndX(mapLoader.getEndPosition().getX());
+            e.setEndY(mapLoader.getEndPosition().getY());
+            e.setSpeed(5);
+            e.setHealth(100);
+            e.setTargetX(e.getX() * 75);
+            e.setTargetY(e.getY() * 75);
+        }
+        /*
         for (Enemy e : enemies) {
             e.setX(mapLoader.getStartPosition().getX());
             e.setY(mapLoader.getStartPosition().getY());
@@ -33,20 +51,37 @@ public class EnemyManager {
             e.setPixelY(mapLoader.getStartPosition().getY() * 75);
             e.setEndX(mapLoader.getEndPosition().getX());
             e.setEndY(mapLoader.getEndPosition().getY());
-            e.setSpeed(3);
+            e.setSpeed(8);
             e.setHealth(100);
+            e.setTargetX(e.getX() * 75);
+            e.setTargetY(e.getY() * 75);
         }
+
+         */
     }
 
     public void loadEnemies() {
         try (BufferedReader br = new BufferedReader(new FileReader("Enemies1"))) {
             String line;
             while ((line = br.readLine()) != null) {
-                enemies.add(new Enemy(15));
+                loadedEnemies.add(new Enemy(15));
             }
             System.out.println(enemies.size());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void spawnEnemy() {
+        long now = System.currentTimeMillis();
+        if (count>= loadedEnemies.size()) {
+            return;
+        }
+        if (now >= loadedEnemies.get(count).getSpawnCooldown() + sinceLastSpawn) {
+            enemies.add(loadedEnemies.get(count));
+            sinceLastSpawn = now;
+            count++;
+        }
+
     }
 }
