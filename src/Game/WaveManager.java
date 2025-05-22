b5aee049473a0721b16c7ad06f4cd4e06f112a3d
jonas <jonassekyra@gmail.com>
2025-05-22 23:01:35 +0200

    Adding nextWave and endGame methods.

--- src/Game/WaveManager.java
+++ src/Game/WaveManager.java
@@ -6,15 +6,22 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+
 import Enemy.*;
+import Level.Level;
 
 public class WaveManager {
     private int currentWave;
     private int numberOfWaves;
     private ArrayList<Wave> waves = new ArrayList<>();
+    private EnemyManager enemyManager;
 
     public WaveManager() {
         loadEnemies();
+    }
+
+    public void setEnemyManager(EnemyManager enemyManager) {
+        this.enemyManager = enemyManager;
     }
 
     public void loadEnemies() {
@@ -23,8 +30,9 @@ public class WaveManager {
             Wave wave = new Wave();
             while ((line = br.readLine()) != null) {
                 Enemy enemy = new Enemy();
-                if (line.startsWith("#")){
-                    if (!wave.getEnemiesInWave().isEmpty()){
+                if (line.startsWith("#")) {
+
+                    if (!wave.getEnemiesInWave().isEmpty()) {
                         waves.add(wave);
                         wave = new Wave();
                     }
@@ -39,6 +47,7 @@ public class WaveManager {
                 //loadedEnemies.add(enemy);
             }
             //System.out.println(enemies.size());
+            numberOfWaves = waves.size();
             System.out.println(waves.size());
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -67,5 +76,23 @@ public class WaveManager {
 
     public void setWaves(ArrayList<Wave> waves) {
         this.waves = waves;
+    }
+
+    public void nextWave() {
+        if (enemyManager.getEnemies().isEmpty() && currentWave + 1 < numberOfWaves) {
+            currentWave++;
+
+        } else {
+            System.out.println("wave hasnt been cleared");
+        }
+    }
+
+    public boolean endGame() {
+        if (currentWave == numberOfWaves) {
+            System.out.println("wave has ended");
+            return true;
+        } else {
+            return false;
+        }
     }
 }
