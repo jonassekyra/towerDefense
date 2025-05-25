48a3c535ebf30da577c706a883157302cb32f7b9
jonas <jonassekyra@gmail.com>
2025-05-25 18:13:08 +0200

    Adding Factory method design pattern for creating towers

--- src/Tower/Tower.java
+++ src/Tower/Tower.java
@@ -2,10 +2,17 @@ package Tower;
 
 import Attack.AttackStrategy;
 import Attack.Projectile;
+import Attack.SingleAttack;
+import Attack.SlowAttack;
 import Enemy.Enemy;
+import Game.ShopManager;
 import Level.Level;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.io.IOException;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,9 +32,7 @@ public abstract class Tower {
     protected long LastAttack = 0;
 
 
-
-
-    public Tower(int damage, BufferedImage image, int rangeX, int rangeY, int posX, int posY, AttackStrategy attackStrategy, long cooldown) {
+    public Tower(int damage, BufferedImage image, int rangeX, int rangeY, int posX, int posY, AttackStrategy attackStrategy, long cooldown, int cost) {
         this.damage = damage;
         this.image = image;
         this.rangeX = rangeX;
@@ -36,6 +41,7 @@ public abstract class Tower {
         this.posY = posY;
         this.attackStrategy = attackStrategy;
         this.cooldown = cooldown;
+        this.cost = cost;
     }
 
     public int getRangeX() {
@@ -115,11 +121,11 @@ public abstract class Tower {
 
     public List<Enemy> enemiesInRange(ArrayList<Enemy> enemies) {
         List<Enemy> results = new ArrayList<>();
-        for (Enemy enemy: enemies){
+        for (Enemy enemy : enemies) {
             int distanceX = enemy.getX() - posX;
             int distanceY = enemy.getY() - posY;
 
-            if (Math.abs(distanceX) < rangeX && Math.abs(distanceY) < rangeY){
+            if (Math.abs(distanceX) < rangeX && Math.abs(distanceY) < rangeY) {
                 results.add(enemy);
             }
 
@@ -127,12 +133,27 @@ public abstract class Tower {
         return results;
     }
 
-    public void canAttack(Level level){
+    public void canAttack(Level level, ShopManager shopManager) {
         //https://gamedev.stackexchange.com/questions/158616/how-do-i-create-a-delay-or-cooldown-timer
         long now = System.currentTimeMillis();
-        if (now >= cooldown + LastAttack){
-            attackStrategy.attack(level, this);
+        if (now >= cooldown + LastAttack) {
+            attackStrategy.attack(level, this,shopManager);
             LastAttack = now;
+        }
+    }
+
+    public static Tower createTower(int type, int posX, int posY) throws IOException {
+        switch (type) {
+            case 1:
+                BufferedImage image = ImageIO.read(Tower.class.getResource("/tiles/Red.png"));
+
+                return new NormalTower(20, image, 2, 2, posX, posY, new SingleAttack(), 750, 50);
+            case 2:
+                BufferedImage image1 = ImageIO.read(Tower.class.getResource("/tiles/Red.png"));
+
+                return new SlowTower(10, image1, 2, 2, posX, posY, new SlowAttack(), 1000, 80);
+            default:
+                return null;
         }
     }
 
