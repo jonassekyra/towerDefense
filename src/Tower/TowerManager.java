50b9478341af93886635e638eb1c2535786cb984
jonas <jonassekyra@gmail.com>
2025-05-25 18:14:51 +0200

    Adding game to TowerManager in order to have access to the coins in attack method

--- src/Tower/TowerManager.java
+++ src/Tower/TowerManager.java
@@ -1,13 +1,16 @@
 package Tower;
 
+import Game.Game;
 import Level.Level;
 
 public class TowerManager {
     private Level level;
+    private Game game;
     private final Tower[][] towers = new Tower[10][10];
 
-    public TowerManager(Level level) {
+    public TowerManager(Level level, Game game) {
         this.level = level;
+        this.game = game;
     }
 
 
@@ -16,7 +19,7 @@ public class TowerManager {
         for (int i = 0; i < level.getMap().length; i++) {
             for (int j = 0; j < level.getMap().length; j++) {
                 if (towers[i][j] != null) {
-                    towers[i][j].canAttack(level);
+                    towers[i][j].canAttack(level, game.getShopManager());
                 }
             }
         }
