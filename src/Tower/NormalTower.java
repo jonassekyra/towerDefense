da809dccedc7a1e29488fdd91b2b3a36552d5ffb
jonas <jonassekyra@gmail.com>
2025-05-25 18:12:09 +0200

    Adding cost to constructor

--- src/Tower/NormalTower.java
+++ src/Tower/NormalTower.java
@@ -8,8 +8,8 @@ import java.sql.Time;
 
 public class NormalTower extends Tower {
 
-    public NormalTower(int damage, BufferedImage image, int rangeX, int rangeY, int posX, int posY, AttackStrategy attackStrategy,long cooldown) {
-        super(damage, image, rangeX, rangeY, posX, posY,attackStrategy,cooldown);
+    public NormalTower(int damage, BufferedImage image, int rangeX, int rangeY, int posX, int posY, AttackStrategy attackStrategy,long cooldown, int cost) {
+        super(damage, image, rangeX, rangeY, posX, posY,attackStrategy,cooldown, cost);
     }
 
     public NormalTower() {
--- src/Tower/SlowTower.java
+++ src/Tower/SlowTower.java
@@ -5,7 +5,7 @@ import Attack.AttackStrategy;
 import java.awt.image.BufferedImage;
 
 public class SlowTower extends Tower {
-    public SlowTower(int damage, BufferedImage image, int rangeX, int rangeY, int posX, int posY, AttackStrategy attackStrategy, long cooldown) {
-        super(damage, image, rangeX, rangeY, posX, posY, attackStrategy, cooldown);
+    public SlowTower(int damage, BufferedImage image, int rangeX, int rangeY, int posX, int posY, AttackStrategy attackStrategy, long cooldown, int cost) {
+        super(damage, image, rangeX, rangeY, posX, posY, attackStrategy, cooldown, cost);
     }
 }
