5e9317b84c46c552e2ec37d996cc98338af1aa20
jonas <jonassekyra@gmail.com>
2025-05-25 17:51:10 +0200

    Adding ShopManager

---
+++ src/Game/ShopManager.java
@@ -0,0 +1,32 @@
+package Game;
+
+import Enemy.Enemy;
+import Panels.EndPanel;
+import Tower.Tower;
+
+public class ShopManager {
+    private int coins;
+
+   public boolean buyTower(Tower tower) {
+       if (coins >= tower.getCost()){
+           coins -= tower.getCost();
+            return true;
+       }else {
+           System.out.println("insufficient funds");
+           return false;
+       }
+
+   }
+
+    public int getCoins() {
+        return coins;
+    }
+
+    public void setCoins(int coins) {
+        this.coins = coins;
+    }
+
+    public void addCoins(Enemy enemy) {
+       coins += enemy.getReward();
+    }
+}
