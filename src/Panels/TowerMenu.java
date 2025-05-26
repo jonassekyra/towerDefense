9b833371109be80ec2e1561cfc84a75c2f5c33bc
jonas <jonassekyra@gmail.com>
2025-05-26 23:12:57 +0200

    Adding restart button and adding buttons to HashMap, so I can easily modify them in gamePanel and adding select and unselect tower methods.

--- src/Panels/TowerMenu.java
+++ src/Panels/TowerMenu.java
@@ -4,12 +4,17 @@ import Game.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.HashMap;
 
 public class TowerMenu extends JPanel {
     JButton normalTower = new JButton("Normal Tower");
-    JButton slowTower = new JButton("Slow Tower");
+    ImageIcon icon = new ImageIcon(getClass().getResource("/Tiles/zelena.png"));
+    JButton slowTower = new JButton("Slow Tower",icon);
     JButton nextWave = new JButton("Next Wave");
+    private JButton restart = new JButton("Restart");
     private String currentlySelectedTower = null;
+    private HashMap<String,JButton> towerButtons = new HashMap<>();
+
 
 
     public String getCurrentlySelectedTower() {
@@ -21,6 +26,8 @@ public class TowerMenu extends JPanel {
     }
 
     public TowerMenu(Game game, MainFrame mainFrame, WaveManager waveManager) {
+        towerButtons.put(normalTower.getText(),normalTower);
+        towerButtons.put(slowTower.getText(),slowTower);
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setVisible(true);
         setBackground(Color.GRAY);
@@ -28,30 +35,86 @@ public class TowerMenu extends JPanel {
 
         add(Box.createRigidArea(new Dimension(0, 20)));
         normalTower.setPreferredSize(new Dimension(200, 30));
+        //normalTower.setBackground(Color.BLUE);
         nextWave.setPreferredSize(new Dimension(200, 30));
 
         slowTower.setPreferredSize(new Dimension(200, 30));
         slowTower.setVerticalAlignment(SwingConstants.CENTER);
+        //slowTower.setOpaque(false);
+        slowTower.setBorderPainted(false);
+        //slowTower.setContentAreaFilled(false);
         slowTower.setHorizontalAlignment(SwingConstants.CENTER);
+        //slowTower.setBackground(Color.BLUE);
         nextWave.setHorizontalAlignment(SwingConstants.CENTER);
         add(nextWave);
         add(Box.createRigidArea(new Dimension(0, 20)));
         add(normalTower);
         add(Box.createRigidArea(new Dimension(0, 20)));
         add(slowTower);
+
+        restart.setPreferredSize(new Dimension(200, 30));
+        restart.setVerticalAlignment(SwingConstants.CENTER);
+        restart.setOpaque(false);
+        restart.setBorderPainted(false);
+        restart.setContentAreaFilled(false);
+        restart.setHorizontalAlignment(SwingConstants.CENTER);
+        add(restart);
+
+        restart.addActionListener(e -> {
+            //MainFrame mainFrame = new MainFrame();
+
+
+        });
+
         normalTower.addActionListener(e -> {
             mainFrame.setGameState(GameState.PLACING_TOWER);
-            currentlySelectedTower = normalTower.getText();
+            //currentlySelectedTower = normalTower.getText();
+            //setCurrentlySelectedTower(normalTower.getText());
+            selectTower(normalTower.getText());
+
 
         });
         slowTower.addActionListener(e -> {
             mainFrame.setGameState(GameState.PLACING_TOWER);
-            currentlySelectedTower = slowTower.getText();
+            //if (game.getShopManager().buyTower())
+            selectTower(slowTower.getText());
+            //currentlySelectedTower = slowTower.getText();
         });
 
         nextWave.addActionListener(e -> {
-            waveManager.setCurrentWave(waveManager.getCurrentWave() + 1);
+            waveManager.nextWave();
         });
+    }
+
+    public HashMap<String, JButton> getTowerButtons() {
+        return towerButtons;
+    }
+
+    public void setTowerButtons(HashMap<String, JButton> towerButtons) {
+        this.towerButtons = towerButtons;
+    }
+
+    public void selectTower(String towerName) {
+        for (JButton button : towerButtons.values()) {
+            button.setBackground(Color.WHITE);
+        }
+
+
+        JButton selectedButton = towerButtons.get(towerName);
+        if (selectedButton != null) {
+            selectedButton.setBackground(Color.RED);
+        }
+
+        currentlySelectedTower = towerName;
+
+
+    }
+
+    public void unselectTower() {
+        currentlySelectedTower = null;
+        for (JButton button : towerButtons.values()) {
+            button.setBackground(Color.WHITE);
+        }
     }
 
 }
