43cec20d983961ee9fa3fd0c54fbd0585b944702
jonas <jonassekyra@gmail.com>
2025-05-26 12:58:17 +0200

    Cleaning up Single and Slow attack by using static methods from AttackUtils.

--- src/Attack/SingleAttack.java
+++ src/Attack/SingleAttack.java
@@ -1,58 +1,40 @@
 package Attack;
 
 import Enemy.Enemy;
+
 import Level.Level;
 import Tower.Tower;
-
+import Game.ShopManager;
 import java.util.ArrayList;
-import java.util.Iterator;
+
+import java.util.List;
 
 public class SingleAttack implements AttackStrategy {
     public SingleAttack() {
     }
 
     @Override
-    public void attack(Level level, Tower tower) {
-
-        int maxProgres = -1;
-        Enemy target = null;
+    public void attack(Level level, Tower tower, ShopManager shopManager) {
         ArrayList<Enemy> enemies = level.getEnemyManager().getEnemies();
         if (enemies.isEmpty()) {
             return;
         }
-        ArrayList<Enemy> enemiesInRange = new ArrayList<>();
-        Iterator<Enemy> it = enemies.iterator();
-        while (it.hasNext()) {
+        List<Enemy> enemiesInRange = AttackUtils.getEnemiesInRange(level, tower);
+        Enemy target = AttackUtils.getFirstTarget(enemiesInRange);
 
-            Enemy enemy = it.next();
-            if (!enemy.isAlive()) {
-                it.remove();
-            }
-            int distanceX = enemy.getPixelX() - (tower.getPosX() * 75);
-            int distanceY = enemy.getPixelY() - (tower.getPosY() * 75);
 
-            if (Math.abs(distanceX) < (tower.getRangeX() * 75) && Math.abs(distanceY) < (tower.getRangeY() * 75)) {
-                enemiesInRange.add(enemy);
-            }
+        if (enemiesInRange.contains(target)) {
+            int[] start = AttackUtils.calculateStart(tower);
+
+            //from IntelliJ Idea
+            assert target != null;
+
+            int[] targetXY = AttackUtils.calculateTarget(target);
+
+            Projectile p = new Projectile(start[0], start[1], 8, targetXY[0], targetXY[1], tower.getDamage(), false);
+            level.getProjectiles().add(p);
         }
 
-        if (enemiesInRange.isEmpty()) {
-            return;
-        }
-        for (Enemy e : enemiesInRange) {
-            if (e.getProgress() > maxProgres){
-                maxProgres = e.getProgress();
-                target = e;
-            }
-        }
-
-        int startX = tower.getPosX() * 75+75/2-5;
-        int startY = tower.getPosY() * 75+75/2-5;
-        int targetX = target.getPixelX()+ 75 / 2 - 5;
-        int targetY = target.getPixelY()+ 75 / 2 - 5;
-        Projectile p = new Projectile(startX,startY,10,targetX,targetY);
-
-        level.getProjectiles().add(p);
 
     }
 }
