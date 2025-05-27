29654123d1e46bac46a42ab1b8766c8647c78f20
jonas <jonassekyra@gmail.com>
2025-05-27 19:02:47 +0200

    Replacing paint method with paintComponent

--- src/Panels/EndPanel.java
+++ src/Panels/EndPanel.java
@@ -5,17 +5,19 @@ import java.awt.*;
 
 public class EndPanel extends JPanel {
     public EndPanel() {
-        setLayout(null);
-        setBackground(Color.WHITE);
+
         setPreferredSize(new Dimension(750, 750));
         repaint();
-
+        this.setBackground(Color.WHITE);
     }
 
-    public void paint(Graphics g) {
-        setBackground(Color.WHITE);
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(Color.black);
+        g2d.setColor(Color.BLACK);
         g2d.setFont(new Font("Arial", Font.BOLD, 20));
         g2d.drawString("Congratulations", 330, 310);
         g2d.drawString("You have finished the game!", 300, 330);
