fb0c460fe4b9e8ad57706fa56451cb8618a3bd64
jonas <jonassekyra@gmail.com>
2025-05-29 16:24:20 +0200

    Adding restart button to the game over panel

--- src/Panels/GameOverPanel.java
+++ src/Panels/GameOverPanel.java
@@ -11,17 +11,23 @@ import java.util.Objects;
 
 public class GameOverPanel extends JPanel {
     private final Image background;
-    private JButton restartButton = new JButton("Restart");
 
     public GameOverPanel(MainFrame mainFrame) {
+        setLayout(null);
         setPreferredSize(new Dimension(750, 750));
+        JButton restartButton = new JButton();
+        restartButton.setSize(525,100);
+        restartButton.setLocation(220,460);
+        restartButton.setContentAreaFilled(false);
+        restartButton.setBorderPainted(true);
+        add(restartButton);
 
         try {
             background = ImageIO.read(Objects.requireNonNull(getClass().getResource("/Game_Over_Screen.png")));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        add(restartButton);
+
         restartButton.addActionListener(e -> {
             mainFrame.restartGame();
             mainFrame.setGameState(GameState.START);
@@ -34,6 +40,6 @@ public class GameOverPanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawImage(background, 0, 0, null);
+        g2d.drawImage(background,0,0, getWidth(), getHeight(), null);
     }
 }
