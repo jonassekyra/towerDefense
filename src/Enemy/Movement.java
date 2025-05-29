d00ce4abeeac6aa72f4ae042f5768f0b0064c4c4
jonas <jonassekyra@gmail.com>
2025-05-29 23:47:12 +0200

    Splitting move method into more methods for cleaner code.

--- src/Enemy/Movement.java
+++ src/Enemy/Movement.java
@@ -4,8 +4,18 @@ import Game.Game;
 import Level.Level;
 import Position.Position;
 
+/**
+ * Moves enemy further on the map.
+ */
 public class Movement {
-
+    /**
+     * Calculates pixel position and tile position.
+     * Checks if the enemy go t to teh next tile.
+     * Sets direction or moves the enemy.
+     * @param enemy Enemy that is supposed to move.
+     * @param level Current level.
+     * @param game Game that stores players lives.
+     */
     public void move(Enemy enemy, Level level, Game game) {
         int tileSize = 75;
         int pixelX = enemy.getPixelX();
@@ -14,51 +24,38 @@ public class Movement {
         int tileX = pixelX / tileSize;
         int tileY = pixelY / tileSize;
 
-        Position currentPosition = new Position(tileX, tileY);
+        Position currentPosition = new Position(tileX,tileY);
 
-        int finishPixX = enemy.getEndX() * tileSize;
-        int finishPixY = enemy.getEndY() * tileSize;
 
         if (pixelX % tileSize == 0 && pixelY % tileSize == 0) {
             enemy.setX(tileX);
             enemy.setY(tileY);
 
         }
-        if (pixelX == finishPixX && pixelY == finishPixY) {
-            System.out.println("end");
-            enemy.doDamage(game);
-            System.out.println(game.getHealth());
-            enemy.setHasEnded(true);
+
+        if (hasReachedEnd(enemy,tileSize,game)){
             return;
         }
 
-        if (enemy.getDirection() == null) {
-            if (level.isInBounds(tileX + 1, tileY) && level.getMap()[tileX + 1][tileY] == 1 && !enemy.getVisitedLocations().contains(currentPosition.right())) {
-                enemy.setDirection(Direction.RIGHT);
-                enemy.getVisitedLocations().add(currentPosition.right());
-                enemy.setTargetX(enemy.getTargetX() + tileSize);
-                enemy.setProgress(enemy.getProgress() + 1);
-            } else if (level.isInBounds(tileX - 1, tileY) && level.getMap()[tileX - 1][tileY] == 1 && !enemy.getVisitedLocations().contains(currentPosition.left())) {
-                enemy.setDirection(Direction.LEFT);
-                enemy.getVisitedLocations().add(currentPosition.left());
-                enemy.setTargetX(enemy.getTargetX() - tileSize);
-                enemy.setProgress(enemy.getProgress() + 1);
-            } else if (level.isInBounds(tileX, tileY + 1) && level.getMap()[tileX][tileY + 1] == 1 && !enemy.getVisitedLocations().contains(currentPosition.down())) {
-                enemy.setDirection(Direction.DOWN);
-                enemy.getVisitedLocations().add(currentPosition.down());
-                enemy.setTargetY(enemy.getTargetY() + tileSize);
-                enemy.setProgress(enemy.getProgress() + 1);
-            } else if (level.isInBounds(tileX, tileY - 1) && level.getMap()[tileX][tileY - 1] == 1 && !enemy.getVisitedLocations().contains(currentPosition.up())) {
-                enemy.setDirection(Direction.UP);
-                enemy.getVisitedLocations().add(currentPosition.up());
-                enemy.setTargetY(enemy.getTargetY() - tileSize);
-                enemy.setProgress(enemy.getProgress() + 1);
 
-            }
+        setDirection(enemy,level,tileX,tileY,currentPosition,tileSize);
+        moveToDirection(enemy);
 
-        }
 
+    }
+
+
+    /**
+     * Moves the enemy in desired direction.
+     * Speed == number of pixels traveled per move.
+     * @param enemy Enemy that currently moves.
+     */
+    private void moveToDirection(Enemy enemy) {
         if (enemy.getDirection() != null) {
+
+            int pixelX = enemy.getPixelX();
+            int pixelY = enemy.getPixelY();
+
             switch (enemy.getDirection()) {
                 case LEFT -> {
                     enemy.setPixelX(pixelX - enemy.getSpeed());
@@ -89,21 +86,63 @@ public class Movement {
                     }
                 }
             }
-
-            if (enemy.getDirection() == null) {
-                int newTileX = enemy.getPixelX() / tileSize;
-                int newTileY = enemy.getPixelY() / tileSize;
-
-                if (newTileX != tileX || newTileY != tileY) {
-                    enemy.setX(newTileX);
-                    enemy.setY(newTileY);
-
-                }
-            }
         }
+    }
 
+    /**
+     * When direction is null it checks where can the enemy go and sets the direction.
+     * @param enemy Enemy that is currently moving.
+     * @param level Current level.
+     * @param tileX X position on a map.
+     * @param tileY Y position ona map.
+     * @param currentPosition Current position of the enemy(not in pixels).
+     * @param tileSize Size of one tile(square).
+     */
+    private void setDirection(Enemy enemy,Level level,int tileX,int tileY,Position currentPosition,int tileSize) {
+        if (enemy.getDirection() == null) {
+            if (level.isInBounds(tileX + 1, tileY) && level.getMap()[tileX + 1][tileY] == 1 && !enemy.getVisitedLocations().contains(currentPosition.right())) {
+                enemy.setDirection(Direction.RIGHT);
+                enemy.getVisitedLocations().add(currentPosition.right());
+                enemy.setTargetX(enemy.getTargetX() + tileSize);
+                enemy.setProgress(enemy.getProgress() + 1);
+            } else if (level.isInBounds(tileX - 1, tileY) && level.getMap()[tileX - 1][tileY] == 1 && !enemy.getVisitedLocations().contains(currentPosition.left())) {
+                enemy.setDirection(Direction.LEFT);
+                enemy.getVisitedLocations().add(currentPosition.left());
+                enemy.setTargetX(enemy.getTargetX() - tileSize);
+                enemy.setProgress(enemy.getProgress() + 1);
+            } else if (level.isInBounds(tileX, tileY + 1) && level.getMap()[tileX][tileY + 1] == 1 && !enemy.getVisitedLocations().contains(currentPosition.down())) {
+                enemy.setDirection(Direction.DOWN);
+                enemy.getVisitedLocations().add(currentPosition.down());
+                enemy.setTargetY(enemy.getTargetY() + tileSize);
+                enemy.setProgress(enemy.getProgress() + 1);
+            } else if (level.isInBounds(tileX, tileY - 1) && level.getMap()[tileX][tileY - 1] == 1 && !enemy.getVisitedLocations().contains(currentPosition.up())) {
+                enemy.setDirection(Direction.UP);
+                enemy.getVisitedLocations().add(currentPosition.up());
+                enemy.setTargetY(enemy.getTargetY() - tileSize);
+                enemy.setProgress(enemy.getProgress() + 1);
 
+            }
 
+        }
+    }
+
+    /**
+     * Calculates the position of the finish in pixels.
+     * checks if the enemy is there and if so it does damage to the player.
+     * @param enemy Current Enemy that is being checked.
+     * @param tileSize Size of a tile.
+     * @param game Game that stores players lives.
+     * @return true if the enemy is at the end.
+     */
+    private boolean hasReachedEnd(Enemy enemy, int tileSize,Game game) {
+        int finishPixX = enemy.getEndX() * tileSize;
+        int finishPixY = enemy.getEndY() * tileSize;
+        if (enemy.getPixelX() == finishPixX && enemy.getPixelY() == finishPixY) {
+            enemy.doDamage(game);
+            enemy.setHasEnded(true);
+            return true;
+        }
+        return false;
     }
 
 }
