36dff65bcc7e97388d6db41a78bbc4a002596053
jonas <jonassekyra@gmail.com>
2025-05-29 16:47:48 +0200

    Adding image as background

--- src/Panels/StartPanel.java
+++ src/Panels/StartPanel.java
@@ -1,21 +1,41 @@
 package Panels;
 
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.util.Objects;
+
 import Game.GameState;
 import Game.MainFrame;
 
 
 public class StartPanel extends JPanel {
+    private final Image background;
     public StartPanel(MainFrame mainFrame) {
         setLayout(null);
         setPreferredSize(new Dimension(750, 750));
+        try {
+            background = ImageIO.read(Objects.requireNonNull(getClass().getResource("/Start_Screen.png")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-        JButton startButton = new JButton("Start");
-        startButton.setBounds(750/2, 750/2, 75, 75);
+
+        JButton startButton = new JButton();
+        startButton.setFocusPainted(true);
+        startButton.setContentAreaFilled(false);
+        startButton.setBounds(145, 485, 680, 100);
         startButton.addActionListener(e -> mainFrame.setGameState(GameState.PLAY));
         this.add(startButton);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawImage(background,0,0, getWidth(), getHeight(), null);
     }
 
 }
